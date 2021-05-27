a3c6ff093d3e8a4dab0f42d080bdab8bec17e5ff
Bodiker <Bodiker86gmail.com>
2021-05-27 22:19:01 +0200

    Osadnicy 3

--- zaloguj.php
+++ zaloguj.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Osadnicy - gra przeglądarkowa</title>
-</head>
-<body>
-    Tylko martwi ujrzeli koniec wojny - Platon <br/><br/>
+<?php
 
-    <form action="zaloguj.php" method="post">
-   
-    Login: <br/> <input type="text" name="login" /> <br/>
-    Hasło: <br/> <input type="password" name="haslo" /> <br/>
-    <input type="submit" value="Zaloguj się" />
-
-    
-    
-    </form>
+$login = $_POST['login'];
+$haslo = $_POST['haslo'];
 
 
-</body>
-</html>
+echo $login."<br/>";
+echo $haslo;
+
+
+?>
