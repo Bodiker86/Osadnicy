69e2c3d9605122c4c2ba293e8c09bed7f26fc535
Bodiker <Bodiker86gmail.com>
2021-05-27 20:59:58 +0200

    git remote add origin https://github.com/Bodiker86/Osadnicy.git

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osadnicy - gra przeglądarkowa</title>
+</head>
+<body>
+    Tylko martwi ujrzeli koniec wojny - Platon <br/><br/>
+
+    <form>
+   
+    Login: <br/> <input type="text" name="login" /> <br/>
+    Hasło: <br/> <input type="password" name="haslo" /> <br/>
+    <input type="submit" value="Zaloguj się" />
+
+    
+    
+    </form>
+
+
+</body>
+</html>
