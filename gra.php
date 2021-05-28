d6d1b5c3be168add6f1172c003a36f68d0afca39
Bodiker <Bodiker86gmail.com>
2021-05-28 20:03:47 +0200

    fin

--- gra.php
+++ gra.php
@@ -1,6 +1,14 @@
 <?php
 
 session_start();
+if (!isset($_SESSION['zalogowany']))
+
+{
+
+    header('Location: index.php');
+    exit();
+
+}
 
 ?>
 <!DOCTYPE html>
--- zaloguj.php
+++ zaloguj.php
@@ -2,6 +2,14 @@
 
 session_start();
 
+if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+{
+
+    header('location: index.php');
+    exit();
+
+}  
+
 require_once "connect.php"; 
 
 $polaczenie =@new mysqli($host,$db_user,$db_password,$db_name);
