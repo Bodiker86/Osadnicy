a76a11c260fdc6a11ac2084bb6cbf7e36d1521b2
Bodiker <Bodiker86gmail.com>
2021-05-28 20:41:01 +0200

    THE  END

--- zaloguj.php
+++ zaloguj.php
@@ -23,8 +23,14 @@ else
     $login = $_POST['login'];
     $haslo = $_POST['haslo'];
 
-    $sql ="SELECT * FROM uzytkownicy WHERE user ='$login' AND pass='$haslo'";
-    if ($rezultat = @$polaczenie->query($sql))
+    $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+    $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
+
+    if ($rezultat = @$polaczenie->query(
+        sprintf("SELECT * FROM uzytkownicy WHERE user ='%s' AND pass='%s'", 
+    mysqli_real_escape_string($polaczenie,$login),
+    mysqli_real_escape_string($polaczenie,$haslo))))
+
     {
         $ilu_userow = $rezultat->num_rows;
         if($ilu_userow>0)
