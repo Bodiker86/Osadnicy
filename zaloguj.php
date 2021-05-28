224397b455c7ce82808fe388efa71901ef35d15a
Bodiker <Bodiker86gmail.com>
2021-05-28 13:45:24 +0200

    it works

--- zaloguj.php
+++ zaloguj.php
@@ -1,11 +1,22 @@
 <?php
 
-$login = $_POST['login'];
-$haslo = $_POST['haslo'];
+require_once "connect.php"; 
 
+$polaczenie =@new mysqli($host,$db_user,$db_password,$db_name);
 
-echo $login."<br/>";
-echo $haslo;
+if ($polaczenie->connect_errno!=0)
+{
+    echo"Error:".$polaczenie->connect_errno."Opis:".$polaczenie->connect_error;
+}
+else
+{
+    $login = $_POST['login'];
+    $haslo = $_POST['haslo'];
+
+    echo "It works! ";
+
+    $polaczenie->close();
+}
 
 
 ?>
